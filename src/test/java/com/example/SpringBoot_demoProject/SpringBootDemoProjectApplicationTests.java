b561c39b13ae4c5f28240285cd9f555d4e44e0e4
Mithila001 <136560946+Mithila001@users.noreply.github.com>
2025-08-05 18:53:25 +0530

    Add ActiveProfiles annotation for test configuration

--- src/test/java/com/example/SpringBoot_demoProject/SpringBootDemoProjectApplicationTests.java
+++ src/test/java/com/example/SpringBoot_demoProject/SpringBootDemoProjectApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.SpringBoot_demoProject;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SpringBootDemoProjectApplicationTests {
 
 	@Test
